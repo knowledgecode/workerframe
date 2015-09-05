e20ec20f254d576474ffd8feb3644339022a00e9
knowledgecode <knowledgecode@gmail.com>
2015-09-05 17:34:43 +0900

    Promise support

--- workerframe.js
+++ workerframe.js
@@ -22,7 +22,7 @@
             if (this === window || typeof fn !== 'function') {
                 throw new TypeError('Failed to construct \'WorkerFrame\'.');
             }
-            frame = function (_origin, _fn) {
+            frame = function (_origin, _uid, _fn) {
                 var s = self, _l = {}, _o = {}, slice = Array.prototype.slice,
                     _on = function (listeners, type, listener) {
                         var el = listeners[type] || [];
@@ -32,6 +32,18 @@
                             listeners[type] = el;
                         }
                         return listener;
+                    },
+                    _off = function (listeners, type, listener) {
+                        var i, el = listeners[type] || [];
+
+                        if (listener) {
+                            i = el.indexOf(listener);
+                            if (i >= 0) {
+                                el.splice(i, 1);
+                            }
+                        } else {
+                            delete listeners[type];
+                        }
                     };
 
                 s.origin = _origin;
@@ -50,23 +62,22 @@
                     return _on(_o, type, listener);
                 };
                 s.off = function (type, listener) {
-                    var i, el = _l[type] || [];
-
-                    i = el.indexOf(listener);
-                    if (i >= 0) {
-                        el.splice(i, 1);
-                    }
+                    _off(_l, type, listener);
+                    _off(_o, type, listener);
                 };
                 s.addEventListener('message', function (evt) {
-                    var type = evt.data._type, data = evt.data._data;
+                    var type = evt.data._type, data = evt.data._data,
+                        callback = function (el) {
+                            el(data, function (res) {
+                                s.message(type, { _uid: _uid, _ok: true, _data: res });
+                            }, function (err) {
+                                s.message(type, { _uid: _uid, _ok: false, _data: err });
+                            });
+                        };
 
                     if (type) {
-                        (_l[type] || []).map(function (el) {
-                            el(data);
-                        });
-                        (_o[type] || []).map(function (el) {
-                            el(data);
-                        });
+                        (_l[type] || []).map(callback);
+                        (_o[type] || []).map(callback);
                         delete _o[type];
                         if (type === 'close') {
                             s.close();
@@ -76,17 +87,20 @@
                 });
                 _fn();
             };
+            this._uid = Math.random().toString(36).slice(2);
             blobURL = URL.createObjectURL(new Blob([
-                '(%s(\'%s\', %s));'
+                '(%s(\'%s\', \'%s\', %s));'
                 .replace('%s', frame.toString())
                 .replace('%s', location.origin.replace('null', 'file://'))
+                .replace('%s', this._uid)
                 .replace('%s', fn.toString())
             ], { type: 'text/javascript' }));
             this._l = {};
             this._o = {};
+            this._p = {};
             this._w = new Worker(blobURL);
             this._w.addEventListener('message', function (evt) {
-                var type = evt.data._type, data = evt.data._data;
+                var type = (evt.data || {})._type || '', data = (evt.data || {})._data;
 
                 switch (type) {
                 case '_l':
@@ -94,7 +108,9 @@
                     break;
                 case '_c':
                     URL.revokeObjectURL(blobURL);
-                    that._l = that._w = undefined;
+                    that._l = that._o = that._p = that._w = undefined;
+                    break;
+                case '':
                     break;
                 default:
                     that._d(type, data);
@@ -113,10 +129,35 @@
                     listeners[type] = el;
                 }
                 return listener;
+            }, _off = function (listeners, type, listener) {
+                var i, el = listeners[type] || [];
+
+                if (listener) {
+                    i = el.indexOf(listener);
+                    if (i >= 0) {
+                        el.splice(i, 1);
+                    }
+                } else {
+                    delete listeners[type];
+                }
             };
 
             this.message = function (type, data, transferList) {
+                var that = this,
+                    promise = new Promise(function (resolve, reject) {
+                        _on(that._p, type, function (res) {
+                            if ((res || {})._uid === that._uid) {
+                                if (res._ok) {
+                                    resolve(res._data);
+                                } else {
+                                    reject(res._data);
+                                }
+                            }
+                        });
+                    });
+
                 this._w.postMessage({ _type: type, _data: data }, transferList);
+                return promise;
             };
             this.close = function () {
                 this.message('close');
@@ -128,21 +169,26 @@
                 return _on(this._o, type, listener);
             };
             this.off = function (type, listener) {
-                var i, el = this._l[type] || [];
-
-                i = el.indexOf(listener);
-                if (i >= 0) {
-                    el.splice(i, 1);
-                }
+                _off(this._l, type, listener);
+                _off(this._o, type, listener);
             };
             this._d = function (type, data) {
-                (this._l[type] || []).map(function (el) {
-                    el(data);
-                });
-                (this._o[type] || []).map(function (el) {
-                    el(data);
-                });
+                var that = this,
+                    callback = function (el) {
+                        if ((data || {})._uid === that._uid) {
+                            el(data._data);
+                        } else {
+                            el(data);
+                        }
+                    };
+
+                (this._l[type] || []).map(callback);
+                (this._o[type] || []).map(callback);
                 delete this._o[type];
+                (this._p[type] || []).map(function (el) {
+                    el(data);
+                });
+                delete this._p[type];
             };
         };
 
